187042ee8748ea69ab03befc0041617a091cdabe
Ana Lúcia Lindroth Dauner <64525911+AnaDauner@users.noreply.github.com>
2020-05-08 15:09:56 +0200

    "Cachematrix" file - result
    R script with the result of the Programming Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions pre-allocate the data and then calculate the inverse of a
+  ## square matrix.
 
-## Write a short comment describing this function
+
+## The function "makeCacheMatrix" creates a random matrix that will pre-allocate
+  ## the data of the inverse matrix.
+  ## The attribute "x" is a square invertible matrix.
+
+# example:
+#matriz <- matrix(c(rnorm(9)), nrow=3, byrow=TRUE)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  
+  get <- function() x
+  
+  setsolve <- function(solve) s <<- solve
+  
+  getsolve <- function() s
+  
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+  
 }
 
+# example:
+#pre_aloc <- makeCacheMatrix(matriz)
 
-## Write a short comment describing this function
+
+## The function "cacheSolve" calculates the inverse of a square matrix.
+  ## The attribute "x" is the list created using the "makeCacheMatrix" function.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    s <- x$getsolve()
+    if(!is.null(s)) {
+      message("getting cached data") ####
+      return(s)
+    }
+    data <- x$get()
+    s <- solve(data, ...)
+    x$setsolve(s)
+    s
+
 }
+
+# example:
+#cacheSolve(pre_aloc)
